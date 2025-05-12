c682b0b294865f5e237c2dbb9e122aea3227ce98
Rafael Tanzer <e12224207@student.tuwien.ac.at>
2025-05-12 21:59:39 +0200

    caching of scopeProvider

--- src/language/service/mini-prob-scope-provider.ts
+++ src/language/service/mini-prob-scope-provider.ts
@@ -1,5 +1,5 @@
-import { AstNodeDescription, AstNodeDescriptionProvider, AstUtils, DefaultScopeProvider, LangiumCoreServices, MapScope, ReferenceInfo, Scope, URI, DefaultIndexManager } from "langium";
-import { Decl, FileImport, Func, isArg, isDecl, isFunc, isFuncCall, isLval, isProgram, Program } from "../generated/ast.js";
+import { AstNodeDescription, AstNodeDescriptionProvider, AstUtils, DefaultScopeProvider, LangiumCoreServices, MapScope, ReferenceInfo, Scope, URI, DefaultIndexManager, Stream, stream } from "langium";
+import { Decl, FileImport, Func, FuncCall, isArg, isDecl, isFunc, isFuncCall, isLval, isProgram, Program } from "../generated/ast.js";
 import { dirname, join, posix } from "path";
 import { SharedMiniProbCache } from "./mini-prob-caching.js";
 import { MiniProbServices } from "../mini-prob-module.js";
@@ -22,43 +22,57 @@ export class MiniProbScopeProvider extends DefaultScopeProvider {
             const program = AstUtils.getContainerOfType(container, isProgram)!;
             var includeFileImports = program.fileImports && program.fileImports.length > 0;
             if (isFuncCall(container)) {
-                // filter Func for body -> only real Func and not ghost Reference(=current input)
-                const descriptions = program.functions.filter(func => func.body).map(func => this.astNodeDescriptionProvider.createDescription(func, func.name));
+                const descriptions = this.descriptionCache.get(AstUtils.getDocument(container).uri, Func, () =>
+                    // filter Func for body -> only real Func and not ghost Reference(=current input)
+                    new MapScope(program.functions.filter(func => func.body)
+                        .map(func => this.astNodeDescriptionProvider.createDescription(func, func.name)))
+                ).getAllElements();
 
                 //check for imported functions
+                var importedDescriptions: Stream<AstNodeDescription> = stream();
                 if (includeFileImports) {
                     const document = AstUtils.getDocument(container);
                     const uri = document.uri;
-                    descriptions.push(...this.getImportedScope(program.fileImports, uri, Func));
+                    importedDescriptions = this.getImportedScope(program.fileImports, uri, Func);
                 }
 
-                return new MapScope(descriptions);
+                return new MapScope(stream(descriptions, importedDescriptions));
             }
             else if (isLval(container)) {
+                const document = AstUtils.getDocument(container);
+
                 const enclosingFunction = AstUtils.getContainerOfType(container, isFunc);
                 if (enclosingFunction) {
-                    const localDeclarations = enclosingFunction.declarations || [];
+
                     const programDeclarations = program.declarations || [];
-                    const descriptions = [...localDeclarations, ...programDeclarations].flatMap(decl =>
+                    const descriptions = this.descriptionCache.get(document.uri, Decl, () => new MapScope(programDeclarations.flatMap(decl =>
+                        decl.names.map(name => this.astNodeDescriptionProvider.createDescription(decl, name))
+                    ))).getAllElements();
+
+                    const localDeclarations = enclosingFunction.declarations || [];
+                    const localDescriptions = [...localDeclarations].flatMap(decl =>
                         decl.names.map(name => this.astNodeDescriptionProvider.createDescription(decl, name))
                     );
+
                     // check for referenced function parameters
                     const localFunctionParameter = enclosingFunction.params?.parameters;
                     if (localFunctionParameter) {
-                        descriptions.push(...localFunctionParameter.map(p => this.astNodeDescriptionProvider.createDescription(p, p.name)));
+                        localDescriptions.push(...localFunctionParameter.map(p => this.astNodeDescriptionProvider.createDescription(p, p.name)));
                     }
+
                     //check for imported declarations
+                    var importedDescriptions: Stream<AstNodeDescription> = stream();
                     if (includeFileImports) {
                         const document = AstUtils.getDocument(container);
                         const uri = document.uri;
-                        descriptions.push(...this.getImportedScope(program.fileImports, uri, Decl));
+                        importedDescriptions = this.getImportedScope(program.fileImports, uri, Decl);
                     }
-                    return new MapScope(descriptions);
+                    return new MapScope(stream(descriptions, localDescriptions, importedDescriptions));
                 } else { // usage of args and lval outside of functions currently not allowed by grammar
                     const programDeclarations = program.declarations || [];
-                    const descriptions = [...programDeclarations].flatMap(decl =>
+                    const descriptions = this.descriptionCache.get(document.uri, Decl, () => new MapScope(programDeclarations.flatMap(decl =>
                         decl.names.map(name => this.astNodeDescriptionProvider.createDescription(decl, name))
-                    );
+                    ))).getAllElements();
                     return new MapScope(descriptions);
                 }
             }
@@ -68,7 +82,7 @@ export class MiniProbScopeProvider extends DefaultScopeProvider {
     }
 
     // only works for imports which are already parsed at elast once (globalScope is cached)
-    private getImportedScope(fileImports: FileImport[], currentUri: URI, targetNodeType: string): AstNodeDescription[] {
+    private getImportedScope(fileImports: FileImport[], currentUri: URI, targetNodeType: string): Stream<AstNodeDescription> {
         const importUris = fileImports.map(f => {
             const filePath = posix.join(dirname(currentUri.path), f.file);
             return currentUri.with({ path: filePath }).toString();
@@ -79,22 +93,14 @@ export class MiniProbScopeProvider extends DefaultScopeProvider {
         if (targetNodeType === Func) {
             const importedFuncDescriptions = this.descriptionCache.get(currentUri, importKey + targetNodeType,
                 () => new MapScope(this.indexManager.allElements(Func, new Set<string>(importUris))));
-            return importedFuncDescriptions.getAllElements().toArray();
+            return importedFuncDescriptions.getAllElements();
         } else if (targetNodeType === Decl) {
             var temp = this.indexManager.allElements(Decl, new Set<string>(importUris));
             const importedDeclDescriptions = this.descriptionCache.get(currentUri, importKey + targetNodeType,
                 () => new MapScope(temp)); //no extra filter necessary ?
-            return importedDeclDescriptions.getAllElements().toArray();
+            return importedDeclDescriptions.getAllElements();
         }
 
-        // if (targetNodeType === Func) {
-        //     var temp = this.indexManager.allElements(Func, new Set<string>(importUris)).toArray();
-        //     return temp;
-        // } else if (targetNodeType === Decl) {
-        //     var temp = this.indexManager.allElements(Decl, new Set<string>(importUris)).filter(decl => !decl.path.includes('function')).toArray();
-        //     return temp;
-        // }
-
-        return [];
+        return stream();
     }
 }
